7777b5a55de7ef0edfa0e922df8d2eca14261e74
partho <partho8181bd@gmail.com>
2016-09-28 18:57:53 +0600

    Initial commit

---
+++ app/src/main/java/com/example/om3g4/shakeit/MainActivity.java
@@ -0,0 +1,132 @@
+package com.example.om3g4.shakeit;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+    ActionBarDrawerToggle toggle;
+    public Button button4;
+
+    public  void init(){
+        button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(MainActivity.this, Location.class);
+                startActivity(i);
+
+
+            }
+        });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        init();
+
+        final Animation animAlpha = AnimationUtils.loadAnimation(this, R.anim.anim_alpha);
+
+
+        Button btnAlpha = (Button) findViewById(R.id.button);
+        Button btnAlpha1 = (Button) findViewById(R.id.button2);
+        Button btnAlpha2 = (Button) findViewById(R.id.button3);
+        Button btnAlpha3 = (Button) findViewById(R.id.button4);
+        Button btnAlpha4 = (Button) findViewById(R.id.button5);
+        Button btnAlpha5 = (Button) findViewById(R.id.button6);
+        Button btnAlpha6 = (Button) findViewById(R.id.button7);
+
+        btnAlpha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.startAnimation(animAlpha);
+                startActivity(new Intent(getApplicationContext(),Preparation.class));
+            }
+        });
+
+
+        btnAlpha1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.startAnimation(animAlpha);
+            }
+        });
+
+        btnAlpha2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.startAnimation(animAlpha);
+            }
+        });
+
+        btnAlpha3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.startAnimation(animAlpha);
+            }
+        });
+
+        btnAlpha4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.startAnimation(animAlpha);
+            }
+        });
+
+        btnAlpha5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //v.startAnimation(animAlpha);
+
+            }
+        });
+
+        btnAlpha6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.startAnimation(animAlpha);
+            }
+        });
+
+
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(toggle.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
+    }
+}
