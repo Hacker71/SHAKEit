ee4ddb4d50b1ea7fd5b135ca82e7f5a62ecb757a
partho <partho8181bd@gmail.com>
2016-10-07 19:32:22 +0600

    comited by partho

--- app/src/main/java/com/example/om3g4/shakeit/During.java
+++ app/src/main/java/com/example/om3g4/shakeit/During.java
@@ -2,12 +2,35 @@ package com.example.om3g4.shakeit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class During extends AppCompatActivity {
+
+    TextCollection textCollection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_during);
+
+        textCollection=new TextCollection();
+
+        TextView during1TV=(TextView)findViewById(R.id.during1TV);
+        TextView during2TV=(TextView)findViewById(R.id.during2TV);
+        TextView during3TV=(TextView)findViewById(R.id.during3TV);
+        TextView during4TV=(TextView)findViewById(R.id.during4TV);
+        TextView during5TV=(TextView)findViewById(R.id.during5TV);
+        TextView during6TV=(TextView)findViewById(R.id.during6TV);
+        TextView during7TV=(TextView)findViewById(R.id.during7TV);
+        TextView during8TV=(TextView)findViewById(R.id.during8TV);
+
+        during1TV.setText(textCollection.during1);
+        during2TV.setText(textCollection.during2);
+        during3TV.setText(textCollection.during3);
+        during4TV.setText(textCollection.during4);
+        during5TV.setText(textCollection.during5);
+        during6TV.setText(textCollection.during6);
+        during7TV.setText(textCollection.during7);
+        during8TV.setText(textCollection.during8);
     }
 }
--- app/src/main/java/com/example/om3g4/shakeit/TextCollection.java
+++ app/src/main/java/com/example/om3g4/shakeit/TextCollection.java
@@ -17,4 +17,18 @@ public class TextCollection {
             "5. Blankets and shoes";
     String before6="Chose and emergency contact- a friend or relative, who lives far away from our own area, to call and check on you after earthquake.";
     String before7="Practice drill- Family member will choose a spot beforehand to reunite if separated during the earthquake.";
+
+
+    String during1="During the earthquake, you should take different approaches based on your location but the important thing is to find a safe place. The idea is to DROP, COVER AND HOLD ON. This is because during the earthquake, the shaking is too violent for crawling or running and if you try to do these, you might get knocked down. So, the best idea is to drop down, find some cover and hold on because this is essentially protect you from flying and falling debris.";
+    String during2="Indoors- Immediately move UNDER DESKS OR TABLES, or stand against the interior walls. AVOID exterior walls, heavy furniture and appliances. If in bed, STAY THERE and cover your head with pillows.";
+    String during3="Outdoors- in case you are outdoors, STAY AWAY from buildings, trees, electric lines and bridges, preferably in open fields.";
+    String during4="In vehicles- try to KEEP AS FAR AWAY from traffic and stops as possible. DO NOT park under bridges and look out for hazzards.";
+    String during5="Mountains- Stay ALERT for any signs of potential land slide and MOVE to high grounds";
+    String during6="Near oceans- before tsunami happens due to large earthquakes, MOVE to high ground.";
+    String during7="Crowded places- DO NOT RUSH towards the exit. DO NOT USE elevators or go nair stairways.";
+    String during8="AVOID!!!! DO NOT-\n" +
+            "1. Run outside or to other rooms- exterior walls are dangerous.   Windows, facades and architectursl details usually collapse first.\n" +
+            "2. Stand in the doorway- in modern houses, the doorways are as strong as the rest of the house so they will not provide shelter and can not protect you from falling debris.\n" +
+            "3. Follow procedures that are shared by any uncertified groups as this may prove fatal, eg- triangle of life.\n" +
+            "AVOID PANICKING- Stay low and cover your head and neck with your hands and arms. Stay calm to think things through and prevent excess use of oxygen.";
 }
