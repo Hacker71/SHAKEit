da57feb1857aa7215bbc814c891a4fd34bf25eca
partho <partho8181bd@gmail.com>
2016-11-15 18:08:36 +0600

    comited by partho

--- app/src/main/java/com/example/om3g4/shakeit/Donate.java
+++ app/src/main/java/com/example/om3g4/shakeit/Donate.java
@@ -1,11 +1,17 @@
 package com.example.om3g4.shakeit;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class Donate extends AppCompatActivity {
 
@@ -16,7 +22,59 @@ public class Donate extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        Button redCrescent=(Button)findViewById(R.id.redCrescent);
+        Button quantum=(Button)findViewById(R.id.quantum);
+        Button islami=(Button)findViewById(R.id.islami);
+        Button salimullah=(Button)findViewById(R.id.salimullah);
+        Button sandhani=(Button)findViewById(R.id.sandhani);
 
+        redCrescent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                callTo("+8801939953720");
+            }
+        });
+        quantum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                callTo("01781603405");
+            }
+        });
+        islami.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                callTo("");
+            }
+        });
+        salimullah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                callTo("");
+            }
+        });
+        sandhani.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                callTo("");
+            }
+        });
+
+    }
+
+    private void callTo(String num) {
+        Intent callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse("tel:" + num));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        startActivity(callIntent);
     }
 
 }
--- app/src/main/java/com/example/om3g4/shakeit/Tools.java
+++ app/src/main/java/com/example/om3g4/shakeit/Tools.java
@@ -1,11 +1,22 @@
 package com.example.om3g4.shakeit;
 
+import android.content.res.AssetFileDescriptor;
+import android.hardware.Camera;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.security.Policy;
+
+import static android.widget.Toast.LENGTH_SHORT;
 
 public class Tools extends AppCompatActivity {
 
@@ -16,7 +27,62 @@ public class Tools extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        Button flashLightBtn=(Button)findViewById(R.id.flashLightBtn);
+        Button strobeLightBtn=(Button)findViewById(R.id.strobeLightBtn);
+        Button sirenBtn=(Button)findViewById(R.id.sirenBtn);
+        Button radioBtn=(Button)findViewById(R.id.radioBtn);
 
+        flashLightBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Camera camera;
+                    camera=Camera.open();
+                    Camera.Parameters p=camera.getParameters();
+                    p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                    camera.setParameters(p);
+                    camera.startPreview();
+                }catch (Exception e){
+                    Toast.makeText(getApplicationContext(), e.toString(), LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        strobeLightBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        sirenBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playTone("tone.mp3");
+            }
+        });
+        radioBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
+
+    void playTone(String tone){
+        try {
+            AssetFileDescriptor afd=getAssets().openFd(tone);
+            MediaPlayer player=new MediaPlayer();
+            player.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+            player.prepare();
+            if(!player.isPlaying()){
+                player.start();
+            }else{
+                player.stop();
+            }
+        } catch (IOException e) {
+            Toast.makeText(getApplicationContext(), e.toString(), LENGTH_SHORT).show();
+        }
     }
 
 }
