a2da07fe94dd615f49a3cb680aa89a567e289590
partho <partho8181bd@gmail.com>
2016-11-15 18:11:03 +0600

    comited by partho

--- app/src/main/java/com/example/om3g4/shakeit/Tools.java
+++ app/src/main/java/com/example/om3g4/shakeit/Tools.java
@@ -51,7 +51,7 @@ public class Tools extends AppCompatActivity {
         strobeLightBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                
             }
         });
         sirenBtn.setOnClickListener(new View.OnClickListener() {
