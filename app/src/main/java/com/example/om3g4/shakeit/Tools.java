b9fda43c372e8006801f64e1d962bae842326803
partho <partho8181bd@gmail.com>
2016-11-15 19:10:57 +0600

    comited by partho

--- app/src/main/java/com/example/om3g4/shakeit/Tools.java
+++ app/src/main/java/com/example/om3g4/shakeit/Tools.java
@@ -1,5 +1,8 @@
 package com.example.om3g4.shakeit;
 
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.content.res.AssetFileDescriptor;
 import android.hardware.Camera;
 import android.media.MediaPlayer;
@@ -9,12 +12,14 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import java.io.IOException;
 import java.security.Policy;
+import java.util.List;
 
 import static android.widget.Toast.LENGTH_SHORT;
 
@@ -51,19 +56,46 @@ public class Tools extends AppCompatActivity {
         strobeLightBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+
             }
         });
         sirenBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                playTone("tone.mp3");
+                playTone("siren.mp3");
             }
         });
         radioBtn.setOnClickListener(new View.OnClickListener() {
+            String fmRadioPackageName="com.sec.android.app.fm";
+            String pack;
             @Override
             public void onClick(View view) {
+                final PackageManager pm = getPackageManager();
 
+                //get a list of installed apps.
+                List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+
+                for (ApplicationInfo packageInfo : packages) {
+                    pack=packageInfo.packageName;
+                    if(pack.contains(".fm")){
+                        fmRadioPackageName=pack;
+                        break;
+                    }
+                    //Log.d("pack", "Installed package :" + packageInfo.packageName);
+                    //Log.d("pack", "Source dir : " + packageInfo.sourceDir);
+                    //Log.d("pack", "Launch Activity :" + pm.getLaunchIntentForPackage(packageInfo.packageName));
+                }
+                // the getLaunchIntentForPackage returns an intent that you can use with startActivity()
+
+
+                try { //to play fm radio
+                    Intent launchIntent = getPackageManager().getLaunchIntentForPackage(fmRadioPackageName);
+                    if (launchIntent != null) {
+                        startActivity(launchIntent);//null pointer check in case package name was not found
+                    }
+                }catch (Exception e){
+                    Toast.makeText(getApplicationContext(), "Unable to play Radio", Toast.LENGTH_LONG).show();
+                }
             }
         });
 
